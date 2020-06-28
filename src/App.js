8fc069ac90a2584cbe7a3e534a94a06c7dc1fbb4
jpatino37 <jpatino3792@gmail.com>
2020-06-27 20:49:07 -0400

    Added eslint with airbnb rules and material navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello :)
-        </p>
-      </header>
+    <div>
+      <Navbar />
     </div>
   );
 }
